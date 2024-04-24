a0de78b493376efb2c63235e22475550b1bec709
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-24 10:49:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,31 @@
 var audio = document.getElementById('audio');
 
 function playSound(soundFile) {
-    stopSound(); 
+    stopSound(); // Stop any var audio = document.getElementById('audio');
+
+function playSound(soundFile) {
+    stopSound();
+    audio.src = 'sounds/' + soundFile;
+    audio.play();
+}
+
+function stopSound() {
+    audio.pause();
+    audio.currentTime = 0;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var buttons = document.getElementsByClassName('btn');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function() {
+            var soundFile = this.getAttribute('onclick').split("'")[1];
+            if (soundFile) {
+                playSound(soundFile);
+            }
+        });
+    }
+});
+
     audio.src = 'sounds/' + soundFile;
     audio.play();
 }
