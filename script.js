bb5bccc25168fcf0685a878254816e5c0ebd012d
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-24 10:14:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const audioElement = document.getElementById('audio');
+
+function playSound(sound) {
+  audioElement.src = `sounds/${sound}`;
+  audioElement.play();
+}
+
+function stopSound() {
+  audioElement.pause();
+  audioElement.currentTime = 0;
+}
