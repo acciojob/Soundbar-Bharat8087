af7a68560aa064ca0b8ffd75bcdb71a0d4b0bdcb
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-24 10:57:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,7 @@
-//your JS code here. If required.
 var audio = document.getElementById('audio');
 
 function playSound(soundFile) {
-    stopSound(); // Stop any var audio = document.getElementById('audio');
-
-function playSound(soundFile) {
     stopSound();
-    audio.src = 'sounds/' + soundFile;
-    audio.play();
-}
-
-function stopSound() {
-    audio.pause();
-    audio.currentTime = 0;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    var buttons = document.getElementsByClassName('btn');
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function() {
-            var soundFile = this.getAttribute('onclick').split("'")[1];
-            if (soundFile) {
-                playSound(soundFile);
-            }
-        });
-    }
-});
-
     audio.src = 'sounds/' + soundFile;
     audio.play();
 }
